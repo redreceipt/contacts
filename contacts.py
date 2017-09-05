7a40583dae82fd9693a62012165ec083c5ca60c6
Michael <micneeley14@gmail.com>
2017-09-05 01:11:55 -0400

    working login function using robobrowser

--- contacts.py
+++ contacts.py
@@ -32,43 +32,6 @@ def _login(user = "", pw = ""):
 	"""
 
 	loginPage = ("https://rock.newspring.cc")
-###	headers={"User-Agent":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36"}
-
-	# create a new session
-###	s = requests.session()
-###	s.headers.update(headers)
-###	page = s.get(loginPage)
-###	soup = BeautifulSoup(page.content, "lxml")
-	#pageHTML = lxml.html.fromstring(page.text)
-
-	# grab ASP.NET __VIEWSTATE strings
-###	VIEWSTATE=soup.find(id="__CVIEWSTATE")['value']
-	#VIEWSTATEGENERATOR=soup.find(id="__VIEWSTATEGENERATOR")['value']
-###	EVENTVALIDATION=soup.find(id="__EVENTVALIDATION")['value']
-
-	#form = {x["name"]: x["value"] for x in soup.findAll(type = "hidden")}	
-###	form = {}
-	
-	#TODO: find all hidden inputs in the login form
-	#pdb.set_trace()
-	"""
-	hidden = soup.findAll(type = "hidden")
-	for x in hidden:
-		if x.has_attr("value"):
-			form[x["name"]] = x["value"]
-	"""
-###	form["__VIEWSTATE"] = VIEWSTATE
-	#TODO: use dotenv to have environment variables
-###	form = {
-	#"__CVIEWSTATE":VIEWSTATE,
-###	"__CVIEWSTATE": VIEWSTATE,
-	#"__VIEWSTATEGENERATOR":VIEWSTATEGENERATOR,
-###	"__EVENTVALIDATION":EVENTVALIDATION,
-###	}
-###	form["ctl17$ctl01$ctl00$tbUserName"] = "michael.neeley"
-###	form["ctl17$ctl01$ctl00$tbPassword"] = "me2ee2cpe1!"
-
-###	response = s.post(loginPage, data = form)
 
 	# testing robobrowser
 	browser = RoboBrowser(history = True)
@@ -81,7 +44,6 @@ def _login(user = "", pw = ""):
 	browser.submit_form(form, submit = form[submitBtn])
 	print("robobrowser: " + browser.url)
 	
-###	print(response.url)	
 	#pdb.set_trace()
 
 if __name__ == "__main__":
